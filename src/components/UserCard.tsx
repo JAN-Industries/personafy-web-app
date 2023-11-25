a04499a0f24160c28d7ab2a10d9629dc22d3f887
Nolan Gelinas <nolangelinas@gmail.com>
2023-11-25 02:58:24 -0500

    Downgrade typescript to support next 14 server components

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -7,7 +7,7 @@ type Props = {
 	user: User;
 };
 
-export default function Card({ user }: Props) {
+export default async function Card({ user }: Props) {
 	const greeting = user.username ? (
 		<div className="flex flex-col items-center p-6 bg-white rounded-lg font-bold text-5xl text-black">
 			Hello {user?.username}!
