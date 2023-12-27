ef6233f4dd0aec2b6a1ab9617ede0b96f141e9e1
Nolan <nolangelinas@gmail.com>
2023-12-27 14:45:01 -0500

    Add accessToken to session

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,15 +1,44 @@
+import { JWT } from "next-auth/jwt";
 import GithubProvider from "next-auth/providers/github";
+import type {
+	GetServerSidePropsContext,
+	NextApiRequest,
+	NextApiResponse,
+} from "next";
+import { getServerSession } from "next-auth";
 
 const options = {
-	// Configure one or more authentication providers
 	secret: process.env.NEXTAUTH_SECRET,
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID ?? "",
 			clientSecret: process.env.GITHUB_SECRET ?? "",
 		}),
-		// ...add more providers here
 	],
+	callbacks: {
+		async jwt({ token, account }: { token: JWT; account: any }) {
+			if (account) {
+				token.accessToken = account.access_token;
+			}
+			return token;
+		},
+		async session({ session, token }: { session: any; token: JWT }) {
+			session.accessToken = token.accessToken;
+			return session;
+		},
+	},
+	jwt: {
+		secret: process.env.NEXTAUTH_SECRET,
+	},
 };
+
+export function getAuth(
+	...args:
+		| [GetServerSidePropsContext["req"], GetServerSidePropsContext["res"]]
+		| [NextApiRequest, NextApiResponse]
+		| []
+) {
+	return getServerSession(...args, options);
+}
 
 export default options;
